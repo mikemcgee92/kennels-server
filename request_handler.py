e5a54e9514895163678f5628e061da70c1df6dca
Mike McGee <mmcgee738@gmail.com>
2025-04-15 21:11:41 -0500

    Added all POST requests

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from views import get_all_animals, get_single_animal, get_single_location, get_all_locations, get_single_employee, get_all_employees, get_single_customer, get_all_customers
+from views import get_all_animals, get_single_animal, get_single_location, get_all_locations, get_single_employee, get_all_employees, get_single_customer, get_all_customers, create_animal, create_location, create_employee, create_customer
 import json
 
 
@@ -53,7 +53,7 @@ class HandleRequests(BaseHTTPRequestHandler):
             if id is not None:
                 response = get_single_animal(id)           
             else:
-                resposne = get_all_animals()
+                response = get_all_animals()
         if resource == "locations":
             if id is not None:
                 response = get_single_location(id)           
@@ -79,14 +79,38 @@ class HandleRequests(BaseHTTPRequestHandler):
         """
         # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
-
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any PUT request.
+        
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+        
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+        
+        # Initialize new animal
+        new_animal = None
+        new_location = None
+        new_employee = None
+        new_customer = None
+        
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+        if resource == "locations":
+            new_location = create_location(post_body)
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+        
+        # Encode the new animal and send in response
+        self.wfile.write(json.dumps(new_animal).encode())
+        self.wfile.write(json.dumps(new_location).encode())
+        self.wfile.write(json.dumps(new_employee).encode())
+        self.wfile.write(json.dumps(new_customer).encode())
 
     def do_PUT(self):
         """Handles PUT requests to the server
